44c3dc341903338bd566f7d9fccdd47bcc7854e9
yoyo <703979707@qq.com>
2019-06-12 11:07:50 +0800

    Update entrypoint.sh
    [update]

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -2,4 +2,4 @@
 
 # Prepare log files and start outputting logs to stdout
 export DJANGO_SETTINGS_MODULE=website.settings
-exec gunicorn -c gunicorn.py website.wsgi:applicatio
+exec gunicorn -c gunicorn.py website.wsgi:application
